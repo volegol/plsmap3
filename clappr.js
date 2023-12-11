a0d088311621e21563cee30b38ae196f4a13b3eb
volegol <104386526+volegol@users.noreply.github.com>
2023-12-11 16:17:25 +0300

    Update clappr.js

--- clappr.js
+++ clappr.js
@@ -5,14 +5,10 @@
             chromeless = false,
             skipCounter, skipInterval, skipOffset, skipText;
 
-            if (Clappr.Browser.isMobile) {
-                autoPlay = false;
-                chromeless = false;
-                // Mobil cihazlar için özel boyutlandırma
-                var mobileWidth = '100%'; // Genişliği ekran genişliğine ayarla
-                var mobileHeight = '300px'; // Yüksekliği sabit bir değere ayarla
-            }
-            
+        if (Clappr.Browser.isMobile) {
+            autoPlay = false;
+            chromeless = false;
+        }
         window.app = {
             clappr: {
                 currentTime: function() {
@@ -23,26 +19,23 @@
                     return window.app.clappr.instance.core.mediaControl.container.buffering;
                 },
                 resizeCallback: function() {
-                    if (Clappr.Browser.isMobile) {
-                        // Mobil cihazlar için özel boyutlandırma
-                        $('div[data-player]').css({
-                            width: mobileWidth,
-                            height: mobileHeight
-                        });
-                    } else {
-                        // Masaüstü için tam ekran boyutlandırma
-                        $('div[data-player]').css({
-                            width: $(window).innerWidth() + 'px',
-                            height: $(window).innerHeight() + 'px'
-                        });
-                    }
-                },
 
+                    /*
+                    window.app.clappr.instance.resize({
+                        width: $(window).innerWidth(),
+                        height: $(window).innerHeight()
+                    });*/
+
+                    $('div[data-player]').css({
+                        width: $(window).innerWidth()+'px',
+                        height: $(window).innerHeight()+'px'
+                    });
+                },
                 options: {
                     autoPlay: autoPlay,
                     disableKeyboardShortcuts: true,
                     disableVideoTagContextMenu: true,
-                    exitFullscreenOnEnd: true,
+                    exitFullscreenOnEnd: false,
                     //height: $(window).innerHeight(),
                     language: "tr-TR",
                     //persistConfig: false,
@@ -58,8 +51,8 @@
                             playback_not_supported: "TarayÃƒÆ’Ã¢â‚¬ÂÃƒâ€šÃ‚Â±cÃƒÆ’Ã¢â‚¬ÂÃƒâ€šÃ‚Â±nÃƒÆ’Ã¢â‚¬ÂÃƒâ€šÃ‚Â±z bu ortamÃƒÆ’Ã¢â‚¬ÂÃƒâ€šÃ‚Â± oynatamÃƒÆ’Ã¢â‚¬ÂÃƒâ€šÃ‚Â±yor, lÃƒÆ’Ã†â€™Ãƒâ€šÃ‚Â¼tfen gÃƒÆ’Ã†â€™Ãƒâ€šÃ‚Â¼ncel bir tarayÃƒÆ’Ã¢â‚¬ÂÃƒâ€šÃ‚Â±cÃƒÆ’Ã¢â‚¬ÂÃƒâ€šÃ‚Â± ile deneyiniz."
                         },
                     },
-                    width: Clappr.Browser.isMobile ? mobileWidth : '100%',
-                    height: Clappr.Browser.isMobile ? mobileHeight : $(window).innerHeight()
+                    width: '100%',
+                    height: $(window).innerHeight()
                 },
             },
             extend: function(defaults, options) {
